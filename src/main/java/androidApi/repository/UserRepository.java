00a1964baf12847c1b2df71c1067ddc7c73e3ab4
Costi <costin.badea.93@gmail.com>
2018-05-02 22:37:19 +0300

    build initial api backbone

---
+++ src/main/java/androidApi/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package androidApi.repository;
+
+import javax.transaction.Transactional;
+
+import androidApi.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+  boolean existsByUsername(String username);
+
+  User findByUsername(String username);
+
+  @Transactional
+  void deleteByUsername(String username);
+
+}

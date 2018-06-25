e90d73e46d725c7a79b6b0b150868dd46c75df11
Costin Badea <costin.badea@endava.com>
2018-06-25 13:17:51 +0300

    cors filter

--- src/main/java/androidApi/security/SimpleCorsFilter.java
+++ src/main/java/androidApi/security/SimpleCorsFilter.java
@@ -1,13 +1,8 @@
-package mainAPI.security;
+package androidApi.security;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;

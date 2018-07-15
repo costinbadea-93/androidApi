440e6bd56746353a5788434bf26cfb189bb02b73
Costi <costin.badea.93@gmail.com>
2018-07-15 18:06:49 +0300

    c9 changes

--- src/main/java/androidApi/repository/CountriesRepository.java
+++ src/main/java/androidApi/repository/CountriesRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 public interface CountriesRepository  extends JpaRepository<Countries, Integer> {
 
-    @Query(value="SELECT * FROM Countries WHERE Countries.country_name=:name",nativeQuery=true)
+    @Query(value="SELECT * FROM Countries WHERE countries.country_name=:name",nativeQuery=true)
     public Countries getCountryId(@Param("name") String name);
 
     @Query("select a from Countries b, Cities a where a.country = b")
